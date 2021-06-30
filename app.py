ace310bee0ac762415514d8f9fc7b0dd67a27db4
chouroukjdidi <chourouk.jdidi@efrei.net>
2021-06-30 10:16:15 +0200

    Route envoi record

--- app.py
+++ app.py
@@ -16,20 +16,23 @@ db= SQLAlchemy(app)
 def index():
     return render_template('index.html')
 
-@app.route('/test', methods = ['GET','POST'])
-def pagetest():
-    return render_template('test.html')
-
 
 @app.route('/refresh', methods = ['POST'])
 def refresh():
     file = request.files['file']
 
     try:
-        file.save(secure_filename(file.filename))
+        file.save('wav_uploaded/en_cours'+secure_filename(file.filename))
         return "Le son à été enregistré"
     except:
         return "Il y a eu une ereur dans l'enregistrement"
+
+
+@app.route('/refresh2', methods = ['POST'])
+def refresh2():
+    print(request.files)
+    return "ok"
+    
 
 
 if __name__ == "__main__":
